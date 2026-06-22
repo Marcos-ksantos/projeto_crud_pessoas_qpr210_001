75ceb207dc800635740ec88b52aa7e9eeaa26382
Tarde Cetafaju <Tarde.Cetafaju@labfies.org.br>
2026-06-22 15:53:29 -0300

    ATUALIZANDO SCRIPT DO FORMULÁRIO

--- js/javascript_pessoas.js
+++ js/javascript_pessoas.js
@@ -6,7 +6,7 @@ const divLista = document.querySelector('#div-lista-pessoas')
 const pessoas = []
 
 //CAPTURANDO O EVENTO SUBMIT DO FORMULÁRIO
-formPessoa.addEventListener('submit', (evt) =>{
+formPessoa.addEventListener('submit', (evt) => {
 
     //INTERROMPE O EFEITO PADRÃO DE RESET APÓS O SUBMIT
     evt.preventDefault()
@@ -15,7 +15,38 @@ formPessoa.addEventListener('submit', (evt) =>{
     const dadosFormPessoa = new FormData(formPessoa)
 
     //CRANDO UM OBJ LITERAL
-    nome: dadosFormPessoa.get('nome') 
-    idade: dadosFormPessoa.get('idade') 
-    renda: dadosFormPessoa.get('renda') 
+    const pessoa = {
+        nome: dadosFormPessoa.get('nome'),
+        idade: dadosFormPessoa.get('idade'),
+        renda: dadosFormPessoa.get('renda')
+    }
+
+    //CHAMANDO A FUNÇÃO addPessoa E PASSANDO O OBJETO LITERAL pessoa
+    addPessoa(pessoa)
+    //LIMPAR FORMULARIO
+    formPessoa.reset()
 })
+
+//CRIANDO A FUNÇÃO ADCIONAR PESSOAS
+const addPessoa = (objPessoa) => {
+
+    //ADICIONANDO O OBJETO LITERAL NO ARRAY PESSOAS
+    pessoas.push(objPessoa)
+
+    //APÓS ADICIONAR O OBJETO LITERAL NO ARRAY CHAMA A FUNÇÃO listPessoas
+    listPessoas()
+}
+
+//FUNÇÃO PARA LISTAR PESSOAS DO ARRAY
+const listPessoas = () => {
+
+    //LIMPAR A DIV LISTA
+    divLista.innerHTML = ''
+
+    //PERCORRE ARRAY pessoas COM O foreach
+    pessoas.forEach((elem, i) => {
+        divLista.innerHTML += `${i} - ${elem.nome} - ${elem.idade} - 
+        ${elem.renda} <br>`
+    })
+}
+
